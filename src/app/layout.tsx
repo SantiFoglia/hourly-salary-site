7d724c101cf2e7e1e1869e0dffa4002328088e02
SantiFoglia <Santiago_Foglia@hotmail.com.ar>
2026-02-14 00:09:27 -0300

    Add AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,6 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2149771442856256"
+          crossOrigin="anonymous"
+        />
+      </head>
+
       <body>
         {/* Header */}
         <header className="header">
